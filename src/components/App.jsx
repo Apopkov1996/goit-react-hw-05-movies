07f0079c30eeda4227390a55140e73547b80c690
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-31 20:33:57 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
+import Home from '../pages/Home';
+import Movies from '../pages/Movies';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [films, setFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState[false];
+
+  const getFilms = async => {
+    try {
+    } catch (error) {}
+  };
+
   return <div>Home</div>;
 };
 
