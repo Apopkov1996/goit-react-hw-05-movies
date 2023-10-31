51b7beebede4748fc37bb6a7b8739e9d922104eb
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-10-31 19:54:44 +0200

    update pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+    </Routes>
   );
 };
---
+++ src/helpers/getMoviesData.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const API_KEY = '7223fe1feb6bb4929f794ea7615387c2';
+
+export const fetchTrendingMovies = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
+};
+
+export const fetchSearchMovies = async query => {
+  const { data } = await axios.get(
+    `search/movie?api_key=${API_KEY}&query=${query}&include_adult=false&page=1`
+  );
+  return data.results;
+};
+
+export const fetchFullInformationMovie = async movieId => {
+  const { data } = await axios.get(`movie/${movieId}?api_key=${API_KEY}`);
+  return data;
+};
+
+export const fetchActorsOfMovie = async movieId => {
+  const { data } = await axios.get(
+    `movie/${movieId}/credits?api_key=${API_KEY}`
+  );
+  return data.cast;
+};
+
+export const fetchReviewsOfMovie = async movieId => {
+  const { data } = await axios.get(
+    `movie/${movieId}/reviews?api_key=${API_KEY}&page=1`
+  );
+  return data.results;
+};
