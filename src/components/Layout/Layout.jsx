4cb6c6be64d18dcf444dc3280d07232499612751
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-11-01 19:00:58 +0200

    update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Layout = () => {
